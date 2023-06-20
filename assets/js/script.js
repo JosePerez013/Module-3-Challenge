6cf6923ab5a759e7d1c47dff4d59be481ffa1336
JosePerez013 <joseperez951@icloud.com>
2023-06-20 03:29:22 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var generateBtn = document.querySelector("#generate");
 
+// This function is called when the user clicks the "Generate Password" button
+// Prompts the user for the password criteria
 function writePassword() {
   var length = prompt(
     "Enter the length of the password (between 8 and 128 characters)"
@@ -16,12 +18,13 @@ function writePassword() {
     alert("Try again. Enter a number between 8 and 128");
     return;
   }
-
+// This is where the user is prompted to select the criteria for the password
   var includeLowerCase = confirm("Include lowercase letters?");
   var includeUpperCase = confirm("Include uppercase letters?");
   var includeNumbers = confirm("Include numbers?");
   var includeSpecialChars = confirm("Include special characters?");
 
+  // If the user does not select any criteria, then an alert is displayed
   if (
     !includeLowerCase &&
     !includeUpperCase &&
@@ -40,11 +43,13 @@ function writePassword() {
     includeSpecialChars: includeSpecialChars,
   };
 
+  // This is where the password is generated
   var password = generatePassword(criteria);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+// This chose the characters for the password
 function generatePassword(criteria) {
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
@@ -69,6 +74,7 @@ function generatePassword(criteria) {
     charset += specialChars;
   }
 
+  // This randomly chooses the characters for the password
   for (var i = 0; i < criteria.length; i++) {
     var randomIndex = Math.floor(Math.random() * charset.length);
     var randomChar = charset[randomIndex];
@@ -78,4 +84,5 @@ function generatePassword(criteria) {
   return password;
 }
 
+// Makes the "Generate Password" button clickable
 generateBtn.addEventListener("click", writePassword);
